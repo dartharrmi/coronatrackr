61d856c3284401967c7c44da702196f499992d9f
Miguel Arroyo <jmiguelarroyo12@gmail.com>
2020-07-13 17:24:59 -0500

    [CT-002]: Create generic class for displaying linear charts

---
+++ lib/ui/widgets/linear_chart.dart
@@ -0,0 +1,146 @@
+import 'package:crownapp/model/response/country_data.dart';
+import 'package:crownapp/utils/text_style.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class NumericLinearChart extends StatelessWidget {
+  final List<NumericChartData> countryData;
+  final String countryName;
+
+  NumericLinearChart({this.countryData, this.countryName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromRGBO(250, 250, 250, 1),
+      padding: const EdgeInsets.all(5.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Card(
+            elevation: 2,
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(3.0),
+            ),
+            child: Column(
+              children: <Widget>[
+                InkWell(
+                  splashColor: Colors.grey.withOpacity(0.4),
+                  onTap: () {
+                    /*Feedback.forLongPress(context);
+                    expandSample(context, list[position], model);*/
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Confirmed',
+                            textAlign: TextAlign.left,
+                            softWrap: true,
+                            textScaleFactor: 1,
+                            overflow: TextOverflow.fade,
+                            style: TextStyle(
+                                fontFamily: 'HeeboMedium',
+                                fontSize: 16.0,
+                                color: const Color.fromRGBO(51, 51, 51, 1),
+                                letterSpacing: 0.2),
+                          ),
+                          Container(
+                              child: Row(
+                            children: <Widget>[
+                              const Padding(
+                                padding: EdgeInsets.only(left: 15),
+                              ),
+                              /*Container(
+                                    height: 24,
+                                    width: 24,
+                                    color: Colors.transparent,
+                                    child: Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          5, 0, 5, 5),
+                                      child: Image.asset(
+                                          'images/fullscreen.png',
+                                          fit: BoxFit.contain,
+                                          height: 20,
+                                          width: 20,
+                                          color: model.backgroundColor),
+                                    ),
+                                  ),*/
+                            ],
+                          )),
+                        ]),
+                  ),
+                ),
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 230,
+                      child: _buildChart(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SfCartesianChart _buildChart() {
+    final isCardView = kIsWeb ? false : true;
+
+    return SfCartesianChart(
+      plotAreaBorderWidth: 0,
+      title: ChartTitle(text: isCardView ? '' : 'Inflation - Consumer price'),
+      legend: Legend(
+          isVisible: isCardView ? false : true,
+          overflowMode: LegendItemOverflowMode.wrap),
+      primaryXAxis: DateTimeAxis(
+          intervalType: DateTimeIntervalType.months,
+          edgeLabelPlacement: EdgeLabelPlacement.shift,
+          dateFormat: DateFormat.yM(),
+          interval: 1,
+          majorGridLines: MajorGridLines(width: 0)),
+      primaryYAxis: NumericAxis(
+          labelFormat: '{value}',
+          numberFormat: Style.numberFormatter,
+          axisLine: AxisLine(width: 0),
+          majorTickLines: MajorTickLines(color: Colors.transparent)),
+      series: getDefaultLineSeries(countryData),
+      tooltipBehavior: TooltipBehavior(enable: true, canShowMarker: false),
+    );
+  }
+
+  List<LineSeries<NumericChartData, DateTime>> getDefaultLineSeries(
+      List<NumericChartData> data) {
+
+    return <LineSeries<NumericChartData, DateTime>>[
+      LineSeries<NumericChartData, DateTime>(
+        animationDuration: 0,
+        enableTooltip: true,
+        dataSource: countryData,
+        xValueMapper: (NumericChartData sales, _) => sales.x,
+        yValueMapper: (NumericChartData sales, _) => sales.y,
+        width: 2,
+        name: countryName,
+        markerSettings: MarkerSettings(isVisible: false),
+      ),
+    ];
+  }
+}
+
+class NumericChartData {
+  NumericChartData(this.x, this.y);
+
+  final DateTime x;
+  final num y;
+}
