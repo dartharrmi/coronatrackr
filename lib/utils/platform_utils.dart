3736e83434e38bcb5eedbe0399b9fef0c4d86e88
Miguel Arroyo <jmiguelarroyo12@gmail.com>
2020-07-15 10:43:37 -0500

    [CT-002]: Create platform_utils file with common constants

--- lib/utils/platform_utils.dart
+++ lib/utils/platform_utils.dart
@@ -3,3 +3,8 @@ import 'package:flutter/foundation.dart';
 const bool isWeb = kIsWeb;
 
 const isCardView = isWeb ? false : true;
+
+const String country_icons_package = "country_pickers";
+
+const String heroCountryFlag = "countryFlag";
+const String heroCountryName = "countryName";
